c8f321a5fa38942df3b8a415b763315cac77a4b0
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-10-01 23:57:43 +0300

    commit tree 431bdf114a564051f424c7e7fa40fc63c3d5b967

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,13 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all
+    if params[:dog_house_id]
+      dog_house = DogHouse.find(params[:dog_house_id])
+      reviews = dog_house.reviews
+    else
+      reviews = Review.all
+    end
+
     render json: reviews, include: :dog_house
   end
 
